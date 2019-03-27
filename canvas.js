cdfe44a17048a373c7f383a3008defb5fe5f03b6
vincefio <vincentfiorilli@gmail.com>
2019-03-26 22:18:58 -0400

    practice

--- canvas.js
+++ canvas.js
@@ -6,8 +6,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
     canvas.height = window.innerHeight
 
     var c = canvas.getContext('2d')
-    c.fillRect(100, 100, 100, 100)
+    // c.fillRect(350, 100, 100, 100)
+    // c.fillRect(100, 100, 100, 100)
+    // c.fillRect(500, 600, 100, 100)
 
-    console.log(canvas)
+    //create 3 random squares
+    for(var i = 0; i < 6; i++){
+      var x = Math.random() * window.innerWidth
+      var y = Math.random() * window.innerHeight
+
+      c.fillRect(x, y, 100, 100)
+    }
+
+    //console.log(canvas)
+
+    c.beginPath()
+    //declare where we want path to start
+    c.moveTo(200, 300)
+    c.lineTo(500, 75)
+    c.lineTo(800, 350)
+    c.stroke()
 
   });
