150f00cf90ebf8a273aa0cffe40d5bc619e62c46
vincefio <vincentfiorilli@gmail.com>
2019-03-28 19:24:41 -0400

    hangman drawing

--- canvas.js
+++ canvas.js
@@ -19,22 +19,39 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
 
     //create 3 random squares
-    for(var i = 0; i < 6; i++){
-      var x = Math.random() * window.innerWidth
-      var y = Math.random() * window.innerHeight
+    // for(var i = 0; i < 6; i++){
+    //   var x = Math.random() * window.innerWidth
+    //   var y = Math.random() * window.innerHeight
 
-      //var colorOne = getRandomColor()
-      c.fillStyle = `${getRandomColor()}`
-      c.fillRect(x, y, 100, 100)
-    }
+    //   //var colorOne = getRandomColor()
+    //   c.fillStyle = `${getRandomColor()}`
+    //   c.fillRect(x, y, 100, 100)
+    // }
 
     //console.log(canvas)
 
+    //head
     c.beginPath()
     //declare where we want path to start
-    c.moveTo(200, 300)
-    c.lineTo(500, 75)
-    c.lineTo(800, 350)
+    c.arc(500, 75, 50, 0, 2 * Math.PI);
+    c.stroke()
+
+    //body
+    c.beginPath()
+    c.moveTo(500, 125)
+    c.lineTo(500, 350)
+    c.stroke()
+
+    //left arm
+    c.beginPath()
+    c.moveTo(400, 125)
+    c.lineTo(500, 200)
+    c.stroke()
+
+    //right arm
+    c.beginPath()
+    c.moveTo(600, 125)
+    c.lineTo(500, 200)
     c.stroke()
 
     //with html canvas we can draw rectangles, lines, arxcs, bezier, images, and text
